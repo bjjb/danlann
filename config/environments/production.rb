f7535278c878a3955fef3e503a85bdd7939254e6
JJ Buckley <jj@bjjb.org>
2009-09-26 00:27:10 +0200

    Only set mailer delivery method to SMTP when the smtp_settings file is available.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,9 +9,9 @@ config.action_controller.consider_all_requests_local = false
 config.action_controller.perform_caching             = true
 config.action_view.cache_template_loading            = true
 
-config.action_mailer.delivery_method = :smtp
 # Load your own config/smtp.yml - which should *not* be checked in!
 if File.exists?(f = File.join(Rails.root, "config", "smtp_settings.yml"))
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = YAML.load(File.read(f))
 end
 
