a394f7597735a1a839ebc1a9b69649305eb9aeda
JJ Buckley <jj@bjjb.org>
2009-11-13 15:39:08 +0100

    Made some environment adjustments
    These are to make sure RMagick loads from a gem in $HOME/gems, and that it can
    be linked against an ImageMagick installation in $HOME/lib. Passenger (for
    example) screws up the environment to ignore the user-defined environment
    variables.
    
    For some reason, require "RMagick" is needed, otherwise the gem custom loading
    fails to find libMagickCore.so. I don't know why, because doing
      ldd $GEM_HOME/gems/rmagick-2.12.2/lib/rmagick/RMagick.so
    shows that all is well.

--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,8 @@
+ENV['PATH'] = "#{ENV['PATH']}:#{ENV['HOME']}/bin"
+ENV['LD_LIBRARY_PATH'] = "#{ENV['HOME']}/lib"
+ENV['GEM_PATH'] = "#{ENV['HOME']}/gems"
+Gem.clear_paths
+
 RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
 
 require File.join(File.dirname(__FILE__), 'boot')
@@ -5,7 +10,8 @@ require File.join(File.dirname(__FILE__), 'boot')
 Rails::Initializer.run do |config|
   config.time_zone = 'UTC'
 
-  config.gem  'rmagick', :lib => 'RMagick'
+  require 'RMagick'
+  config.gem  'rmagick', :lib => 'RMagick', :version => '>=2.12.2'
   config.gem  'authlogic', :version => '>=2.1.3'
   config.gem  'will_paginate', :version => '>=2.0.3'
   config.gem  'bluecloth', :version => '>=2.0.5'
